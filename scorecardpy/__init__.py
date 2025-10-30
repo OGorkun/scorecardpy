0396c1a6715fc4aeb938f2670cf5b9028f74f999
OGorkun <oogorkun@gmail.com>
2025-10-30 16:21:35 +0100

    __init__.py changed to relative imports

--- scorecardpy/__init__.py
+++ scorecardpy/__init__.py
@@ -1,31 +1,35 @@
 # -*- coding:utf-8 -*- 
 
-from scorecardpy.germancredit import germancredit
-from scorecardpy.split_df import split_df
-from scorecardpy.info_value import iv
-# from .info_ent_indx_gini import (ig, ie)
-from scorecardpy.var_filter import var_filter
-from scorecardpy.woebin import (woebin, woebin_ply, woebin_plot, woebin_adj, vars_iv, vars_filter)
-from scorecardpy.perf import (perf_eva, perf_psi, iv_group, gini_vars, gini_over_time, score_ranges, score_distr, psi, psi_vars, psi_over_time)
-from scorecardpy.scorecard import (scorecard, scorecard_ply)
-from scorecardpy.one_hot import one_hot
-#from scorecardpy.vif import vif
-from scorecardpy.extension import (miss_heatmap, expl_analysis, nan_treatment, var_distr, hhi)
-from scorecardpy.calibration import (pd_from_score, calibration)
-
-
-__version__ = '1.0'
-
-__all__ = (
-    germancredit,
-    split_df,
-    iv,
-    var_filter,
-    woebin, woebin_ply, woebin_plot, woebin_adj, vars_iv, vars_filter,
-    perf_eva, perf_psi, iv_group, gini_vars, gini_over_time, score_ranges, score_distr, psi, psi_vars, psi_over_time,
-    scorecard, scorecard_ply,
-    one_hot,
-    #vif,
-    miss_heatmap, expl_analysis, nan_treatment, var_distr, hhi,
-    pd_from_score, calibration
+from .germancredit import germancredit
+from .split_df import split_df
+from .info_value import iv
+# from .info_ent_indx_gini import ig, ie
+from .var_filter import var_filter
+from .woebin import (
+    woebin, woebin_ply, woebin_plot, woebin_adj, vars_iv, vars_filter
 )
+from .perf import (
+    perf_eva, perf_psi, iv_group, gini_vars, gini_over_time,
+    score_ranges, score_distr, psi, psi_vars, psi_over_time
+)
+from .scorecard import scorecard, scorecard_ply
+from .one_hot import one_hot
+# from .vif import vif
+from .extension import (
+    miss_heatmap, expl_analysis, nan_treatment, var_distr, hhi
+)
+from .calibration import pd_from_score, calibration
+
+__version__ = "1.0"
+
+__all__ = [
+    "germancredit", "split_df",
+    "iv", "var_filter",
+    "woebin", "woebin_ply", "woebin_plot", "woebin_adj", "vars_iv", "vars_filter",
+    "perf_eva", "perf_psi", "iv_group", "gini_vars", "gini_over_time",
+    "score_ranges", "score_distr", "psi", "psi_vars", "psi_over_time",
+    "scorecard", "scorecard_ply",
+    "one_hot",
+    "miss_heatmap", "expl_analysis", "nan_treatment", "var_distr", "hhi",
+    "pd_from_score", "calibration",
+]
